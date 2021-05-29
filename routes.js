54c35735415e21ae46eee2ace28ffd3a73b6ba7c
unknown <badrusalsyihab@gmail.com>
2021-05-29 23:25:26 +0700

    add route geData, Store & delete

--- routes.js
+++ routes.js
@@ -6,4 +6,20 @@ module.exports = function (app) {
     app.route('/')
         .get('myJson.index');
 
+    app.route('/get')
+        .get(myJson.getData);
+
+    app.route('/get/:id')
+        .get(myJson.find);
+
+    app.route('/add')
+        .post(myJson.addData);
+
+    app.route('/update')
+        .put(myJson.updateData);
+
+    app.route('/delete')
+        .delete(myJson.removeData);
+
+
 }
