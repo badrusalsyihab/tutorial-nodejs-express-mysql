33f65d022bc12dbce2e94b4623ebf0084b794dcd
unknown <badrusalsyihab@gmail.com>
2021-05-29 20:21:44 +0700

    call route in server

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ const app = express();
 //parser
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+
+//call route
+var routes = require('./routes');
+
+//running server port 3000
 app.listen(3000, () => {
     console.log(`Server started on port`);
 });
